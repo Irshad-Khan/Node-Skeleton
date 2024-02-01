cfa4e4574958aa98f5a4ce70163fe74189186d84
muhammad.irshad <muhammad.irshad@pitb.gov.pk>
2024-02-01 09:58:46 +0500

    initial commit

---
+++ env-example.js
@@ -0,0 +1,14 @@
+module.exports = {
+    APP: {
+        APP_NAME: "NODE LEARNING",
+        APP_ENV: "dev",
+        HOST: "http://localhost:3000",
+    },
+    DATABASE: {
+        DBHOST: "localhost",
+        USER: "root",
+        DATABASE_NAME: "",
+        PASSWORD: "",
+        DATABASETYPE: 'mysql'
+    }
+};
